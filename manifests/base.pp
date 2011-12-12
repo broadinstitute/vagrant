38345740760e18274781267559b785d1173ceb26
Jeff Loiselle <jeff@broadinstitute.org>
2011-12-12 18:25:24 -0500

    added first pass at dev configuration

---
+++ manifests/base.pp
@@ -0,0 +1,51 @@
+exec { "apt-get update":
+  command => "/usr/bin/apt-get update",
+}
+
+Package { require => Exec["apt-get update"] }
+File { require => Exec["apt-get update"] }
+
+package { "vim":
+  ensure => present,
+}
+
+package { "apache2":
+  ensure => present,
+}
+
+package { "php5":
+  ensure => present,
+}
+
+package { "mysql-server":
+  ensure => present,
+}
+
+package { "php5-mysql":
+  ensure => present,
+}
+
+package { "php-pear":
+  ensure => present,
+}
+
+package { "php5-xdebug":
+  ensure => present,
+}
+
+service { "apache2":
+  ensure => running,
+  enable => true,
+  subscribe => [File["/etc/apache2/mods-enabled/rewrite.load"], File["/etc/apache2/sites-available/default"]],
+}
+
+file { "/etc/apache2/mods-enabled/rewrite.load":
+  ensure => link,
+  target => "/etc/apache2/mods-available/rewrite.load",
+}
+
+file { "/etc/apache2/sites-available/default":
+  ensure => present,
+  source => "/vagrant/manifests/default",
+}  
+
